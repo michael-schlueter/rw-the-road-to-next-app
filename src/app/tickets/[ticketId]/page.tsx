03dbb362ce3e0a667a0b480c3607781bf7c5166b
rulo <schluet0r4life@gmail.com>
2024-11-28 20:31:45 +0100

    Not-Found Route

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,9 +1,7 @@
-import Placeholder from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
 import TicketItem from "@/features/ticket/components/ticket-item";
 import getTicket from "@/features/ticket/queries/get-ticket";
-import { ticketsPath } from "@/paths";
-import Link from "next/link";
+
+import { notFound } from "next/navigation";
 
 type TicketPageProps = {
   params: Promise<{
@@ -16,16 +14,7 @@ export default async function TicketPage({ params }: TicketPageProps) {
   const ticket = await getTicket(ticketId);
 
   if (!ticket) {
-    return (
-      <Placeholder
-        label="Ticket not found"
-        button={
-          <Button asChild variant="outline">
-            <Link href={ticketsPath()}>Go to Tickets</Link>
-          </Button>
-        }
-      />
-    );
+    return notFound();
   }
 
   return (
