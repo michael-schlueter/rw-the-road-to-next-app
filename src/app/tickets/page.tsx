af5073cb60dfc6e09d27c743632da25e1690b9b4
rulo <schluet0r4life@gmail.com>
2024-11-28 17:20:13 +0100

    Data-Fetching in Server Components

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,10 @@
-"use client";
-
 import Heading from "@/components/heading";
 import TicketItem from "@/features/ticket/components/ticket-item";
-import { useEffect, useState } from "react";
-import { Ticket } from "@/features/ticket/types";
 import getTickets from "@/features/ticket/queries/get-tickets";
 
-export default function TicketsPage() {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
+export default async function TicketsPage() {
+  const tickets = await getTickets();
 
-  useEffect(() => {
-    async function fetchTickets() {
-      const result = await getTickets();
-      setTickets(result);
-    }
-
-    fetchTickets();
-  }, []);
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
