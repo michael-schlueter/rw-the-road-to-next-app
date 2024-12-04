b8c05ba0b313fb25f2b19e172be5e1bbe0bc5354
rulo <schluet0r4life@gmail.com>
2024-12-04 16:12:40 +0100

    Forn Validation

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -4,20 +4,34 @@ import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
-export async function upsertTicket(id: string | undefined, _actionState: { message: string }, formData: FormData) {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
-
-  await prisma.ticket.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
+export async function upsertTicket(
+  id: string | undefined,
+  _actionState: { message: string },
+  formData: FormData
+) {
+  const upsertTicketSchema = z.object({
+    title: z.string().min(1).max(191),
+    content: z.string().min(1).max(1024),
   });
+
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title"),
+      content: formData.get("content"),
+    });
+
+    await prisma.ticket.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+  } catch (error) {
+    return { message: "Something went wrong" };
+  }
 
   revalidatePath(ticketsPath());
 
