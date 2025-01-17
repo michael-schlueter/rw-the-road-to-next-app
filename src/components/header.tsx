3f23fcdfbebd44ba36fe9fde1402429b7e9139a6
rulo <schluet0r4life@gmail.com>
2025-01-17 19:27:58 +0100

    Sign Out

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 import { homePath, signInPath, signUpPath, ticketsPath } from "@/paths";
-import { LucideKanban } from "lucide-react";
+import { LucideKanban, LucideLogOut } from "lucide-react";
 import { buttonVariants } from "./ui/button";
 import Link from "next/link";
 import ThemeSwitcher from "./themes/theme-switcher";
+import { signOut } from "@/features/auth/actions/sign-out";
+import SubmitButton from "./form/submit-button";
 
 export default function Header() {
   const navItems = (
@@ -25,6 +27,9 @@ export default function Header() {
       >
         Sign In
       </Link>
+      <form action={signOut}>
+        <SubmitButton label="Sign Out" icon={<LucideLogOut />} />
+      </form>
     </>
   );
 
