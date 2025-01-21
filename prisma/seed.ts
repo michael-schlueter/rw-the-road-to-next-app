e6fb9e60c61e8f90cad78b2e32802ea9f3e2bd20
rulo <schluet0r4life@gmail.com>
2025-01-21 13:37:15 +0100

    One-to-Many Relation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,18 @@
+import { hash } from "@node-rs/argon2";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "user",
+    email: "schluet0r4life@gmail.com",
+  },
+];
 
 const tickets = [
   {
@@ -31,21 +43,34 @@ async function seed() {
   console.log("DB Seed: Started ...");
 
   await prisma.ticket.deleteMany();
+  await prisma.user.deleteMany();
 
-//   for (const ticket of tickets) {
-//     await prisma.ticket.create({
-//         data: ticket,
-//     });
-//   }
+  //   for (const ticket of tickets) {
+  //     await prisma.ticket.create({
+  //         data: ticket,
+  //     });
+  //   }
 
-//   const promises = tickets.map((ticket) => prisma.ticket.create({
-//     data: ticket,
-//   }))
+  //   const promises = tickets.map((ticket) => prisma.ticket.create({
+  //     data: ticket,
+  //   }))
 
-//   await Promise.all(promises);
+  //   await Promise.all(promises);
+
+  const passwordHash = await hash("geheimnis");
+
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({
+      ...user,
+      passwordHash,
+    })),
+  });
 
   await prisma.ticket.createMany({
-    data: tickets,
+    data: tickets.map((ticket) => ({
+      ...ticket,
+      userId: dbUsers[0].id,
+    })),
   });
 
   const t1 = performance.now();
