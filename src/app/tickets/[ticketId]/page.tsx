cf8fa440bb41b7fc694bfb1aaa70c8e7c7c438cd
rulo <schluet0r4life@gmail.com>
2025-01-22 13:58:21 +0100

    Breadcrumbs

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,5 +1,8 @@
+import Breadcrumbs from "@/components/breadcrumbs";
+import { Separator } from "@/components/ui/separator";
 import TicketItem from "@/features/ticket/components/ticket-item";
 import getTicket from "@/features/ticket/queries/get-ticket";
+import { homePath } from "@/paths";
 
 import { notFound } from "next/navigation";
 
@@ -18,8 +21,19 @@ export default async function TicketPage({ params }: TicketPageProps) {
   }
 
   return (
-    <div className="flex justify-center">
-      <TicketItem ticket={ticket} isDetail />
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath() },
+          { title: ticket.title },
+        ]}
+      />
+
+      <Separator />
+
+      <div className="flex justify-center">
+        <TicketItem ticket={ticket} isDetail />
+      </div>
     </div>
   );
 }
