165a258735c33544cea613aec28f9cc090780a85
rulo <schluet0r4life@gmail.com>
2025-01-23 18:48:07 +0100

    Debounce Requests

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Input } from "./ui/input";
+import { useDebouncedCallback } from "use-debounce";
 
 type SearchInputProps = {
   placeholder: string;
@@ -12,19 +13,22 @@ export default function SearchInput({ placeholder }: SearchInputProps) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    const params = new URLSearchParams(searchParams);
+  const handleSearch = useDebouncedCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      const params = new URLSearchParams(searchParams);
 
-    if (value) {
-      params.set("search", value);
-    } else {
-      params.delete("search");
-    }
+      if (value) {
+        params.set("search", value);
+      } else {
+        params.delete("search");
+      }
 
-    replace(`${pathname}?${params.toString()}`, {
-      scroll: false,
-    });
-  };
+      replace(`${pathname}?${params.toString()}`, {
+        scroll: false,
+      });
+    },
+    250
+  );
   return <Input placeholder={placeholder} onChange={handleSearch} />;
 }
