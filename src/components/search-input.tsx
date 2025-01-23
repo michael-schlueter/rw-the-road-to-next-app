4e5fd4ac75621ce401a6bdeab71477cb35a39d04
rulo <schluet0r4life@gmail.com>
2025-01-23 17:18:26 +0100

    useSearchParams

---
+++ src/components/search-input.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Input } from "./ui/input";
+
+type SearchInputProps = {
+  placeholder: string;
+};
+
+export default function SearchInput({ placeholder }: SearchInputProps) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+
+    replace(`${pathname}?${params.toString()}`, {
+      scroll: false,
+    });
+  };
+  return <Input placeholder={placeholder} onChange={handleSearch} />;
+}
