5d0bb9cf4f67600e3673ae0d1d458efa3f1d4295
rulo <schluet0r4life@gmail.com>
2025-01-29 11:18:29 +0100

    Sequential Data Fetching

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -21,6 +21,8 @@ import { TicketWithMetadata } from "../types";
 import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import Comments from "@/features/comment/components/comments";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type TicketItemProps = {
   ticket: TicketWithMetadata;
@@ -116,7 +118,19 @@ export default async function TicketItem({
         </div>
       </div>
 
-      {isDetail ? <Comments ticketId={ticket.id} /> : null}
+      {isDetail ? (
+        <Suspense
+          fallback={
+            <div className="flex flex-col gap-y-4">
+              <Skeleton className="h-[250px] w-full" />
+              <Skeleton className="h-[80px] ml-8" />
+              <Skeleton className="h-[80px] ml-8" />
+            </div>
+          }
+        >
+          <Comments ticketId={ticket.id} />
+        </Suspense>
+      ) : null}
     </div>
   );
 }
