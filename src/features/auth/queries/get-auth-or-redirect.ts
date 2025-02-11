b4515e0e6120c12a1fac2969aec1460754b0429f
rulo <schluet0r4life@gmail.com>
2025-02-11 18:23:52 +0100

    Redirect if not Verified

--- src/features/auth/queries/get-auth-or-redirect.ts
+++ src/features/auth/queries/get-auth-or-redirect.ts
@@ -1,12 +1,16 @@
 import { redirect } from "next/navigation";
 import { getAuth } from "./get-auth";
-import { signInPath } from "@/paths";
+import { emailVerificationPath, signInPath } from "@/paths";
 
 export async function getAuthOrRedirect() {
   const auth = await getAuth();
 
   if (!auth.user) {
     redirect(signInPath());
+  }
+
+  if (!auth.user.emailVerified) {
+    redirect(emailVerificationPath());
   }
 
   return auth;
