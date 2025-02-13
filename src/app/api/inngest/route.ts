9186e7e33875e52f3a70f2bbb40cdc89e55c9bf4
rulo <schluet0r4life@gmail.com>
2025-02-13 11:10:45 +0100

    Deactivate Inngest

--- src/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest";
-import { passwordResetFunction } from "@/features/password/events/event-password-reset";
-import { emailVerificationFunction } from "@/features/auth/events/event-email-verification";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [passwordResetFunction, emailVerificationFunction], 
-});
--- src/lib/inngest.ts
+++ src/lib/inngest.ts
@@ -1,13 +1,13 @@
-import { EmailVerificationEventArgs } from "@/features/auth/events/event-email-verification";
-import { PasswordResetEventArgs } from "@/features/password/events/event-password-reset";
-import { EventSchemas, Inngest } from "inngest";
+// import { EmailVerificationEventArgs } from "@/features/auth/events/event-email-verification";
+// import { PasswordResetEventArgs } from "@/features/password/events/event-password-reset";
+// import { EventSchemas, Inngest } from "inngest";
 
-type Events = {
-  "app/password.password-reset": PasswordResetEventArgs;
-  "app/auth.sign-up": EmailVerificationEventArgs;
-};
+// type Events = {
+//   "app/password.password-reset": PasswordResetEventArgs;
+//   "app/auth.sign-up": EmailVerificationEventArgs;
+// };
 
-export const inngest = new Inngest({
-  id: "the-road-to-next",
-  schemas: new EventSchemas().fromRecord<Events>(),
-});
+// export const inngest = new Inngest({
+//   id: "the-road-to-next",
+//   schemas: new EventSchemas().fromRecord<Events>(),
+// });
