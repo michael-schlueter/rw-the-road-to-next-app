85833773a22d03d1b7bd466f3e0ab5c05d304a2e
rulo <schluet0r4life@gmail.com>
2025-03-19 10:29:11 +0100

    Protected Admin API

--- src/features/membership/actions/delete-membership.ts
+++ src/features/membership/actions/delete-membership.ts
@@ -4,6 +4,7 @@ import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect"
 import { getMemberships } from "../queries/get-memberships";
 import { toActionState } from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
+import { setCookieByKey } from "@/actions/cookies";
 
 export async function deleteMembership({
   userId,
@@ -12,16 +13,57 @@ export async function deleteMembership({
   userId: string;
   organizationId: string;
 }) {
-  await getAuthOrRedirect();
+  const { user } = await getAuthOrRedirect();
 
   const memberships = await getMemberships(organizationId);
 
+  // Check if it's the last membership
   const isLastMembership = (memberships ?? []).length <= 1;
 
   if (isLastMembership) {
     return toActionState(
       "ERROR",
       "You cannot delete the last membership of an organization"
+    );
+  }
+
+  // Check if membership exists
+
+  const targetMembership = (memberships ?? []).find(
+    (membership) => membership.userId === userId
+  );
+
+  if (!targetMembership) {
+    return toActionState("ERROR", "Membership not found");
+  }
+
+  // Check if user is deleting last admin
+
+  const adminMemberships = (memberships ?? []).filter(
+    (membership) => membership.membershipRole === "ADMIN"
+  );
+  const removesAdmin = targetMembership.membershipRole === "ADMIN";
+  const isLastAdmin = adminMemberships.length <= 1;
+
+  if (removesAdmin && isLastAdmin) {
+    return toActionState(
+      "ERROR",
+      "You cannot delete the last admin of an organization"
+    );
+  }
+
+  // Check if user is deleting him/herself
+  const myMembership = (memberships ?? []).find(
+    (membership) => membership.userId === user?.id
+  );
+
+  const isMyself = user.id === userId;
+  const isAdmin = myMembership?.membershipRole === "ADMIN";
+
+  if (!isMyself && !isAdmin) {
+    return toActionState(
+      "ERROR",
+      "You can only delete memberships as an admin"
     );
   }
 
@@ -34,5 +76,10 @@ export async function deleteMembership({
     },
   });
 
-  return toActionState("SUCCESS", "The membership has been deleted");
+  await setCookieByKey(
+    "toast",
+    isMyself
+      ? "You have left the organization"
+      : "The membership has been deleted"
+  );
 }
--- src/features/organization/actions/delete-organization.ts
+++ src/features/organization/actions/delete-organization.ts
@@ -1,15 +1,15 @@
 "use server";
 
-import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 import { getOrganizationsByUser } from "../queries/get-organizations-by-user";
 import {
   fromErrorToActionState,
   toActionState,
 } from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
+import { getAdminOrRedirect } from "@/features/membership/queries/get-admin-or-redirect";
 
 export async function deleteOrganization(organizationId: string) {
-  await getAuthOrRedirect();
+  await getAdminOrRedirect(organizationId);
 
   try {
     // Check if user is member of the organization he wants to delete
