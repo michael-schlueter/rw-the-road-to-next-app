c5316d2c9bf0b9ebe263e2647dd80a1c249c8bd0
rulo <schluet0r4life@gmail.com>
2025-03-27 18:30:14 +0100

    Upload Files

--- src/features/attachments/components/attachment-create-form.tsx
+++ src/features/attachments/components/attachment-create-form.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { useActionState } from "react";
+import { createAttachments } from "../actions/create-attachments";
+import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
+import Form from "@/components/form/form";
+import { Input } from "@/components/ui/input";
+import { ACCEPTED } from "../constants";
+import FieldError from "@/components/form/field-error";
+import SubmitButton from "@/components/form/submit-button";
+
 type AttachmentCreateFormProps = {
   ticketId: string;
 };
@@ -5,6 +16,22 @@ type AttachmentCreateFormProps = {
 export default function AttachmentCreateForm({
   ticketId,
 }: AttachmentCreateFormProps) {
-  // TODO: upload files as attachments
-  return null;
+  const [actionState, action] = useActionState(
+    createAttachments.bind(null, ticketId),
+    EMPTY_ACTION_STATE
+  );
+
+  return (
+    <Form action={action} actionState={actionState}>
+      <Input
+        name="files"
+        id="files"
+        type="file"
+        multiple
+        accept={ACCEPTED.join(",")}
+      />
+      <FieldError actionState={actionState} name="files" />
+      <SubmitButton label="Upload" />
+    </Form>
+  );
 }
---
+++ src/features/attachments/constants.ts
@@ -0,0 +1,8 @@
+export const ACCEPTED = [
+  "image/png",
+  "image/jpg",
+  "image/jpeg",
+  "application/pdf",
+];
+
+export const MAX_SIZE = 4;
