c151a8c313715ace3d69c0f214653e83d7fec2f9
rulo <schluet0r4life@gmail.com>
2025-04-03 14:15:11 +0200

    Sign A Polymorphic Relation

--- src/app/api/aws/s3/attachments/[attachmentId]/route.ts
+++ src/app/api/aws/s3/attachments/[attachmentId]/route.ts
@@ -5,6 +5,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3 } from "@/lib/aws";
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { generateS3Key } from "@/features/attachments/utils/generate-s3-key";
+import { getOrganizationIdByAttachment } from "@/features/attachments/utils/helpers";
 
 export async function GET(
   request: NextRequest,
@@ -20,16 +21,33 @@ export async function GET(
     },
     include: {
       ticket: true,
+      comment: {
+        include: {
+          ticket: true,
+        },
+      },
     },
   });
+
+  const subject = attachment.ticket ?? attachment.comment;
+
+  if (!subject) {
+    throw new Error("Subject not found");
+  }
+
+  const organizationId = getOrganizationIdByAttachment(
+    attachment.entity,
+    subject
+  );
 
   const presignedUrl = await getSignedUrl(
     s3,
     new GetObjectCommand({
       Bucket: process.env.AWS_BUCKET_NAME,
       Key: generateS3Key({
-        organizationId: attachment.ticket.organizationId,
-        ticketId: attachment.ticket.id,
+        organizationId,
+        entityId: subject.id,
+        entity: attachment.entity,
         fileName: attachment.name,
         attachmentId: attachment.id,
       }),
