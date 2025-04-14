3aa45871f742dd7aced233375f21f8e06849e5e2
rulo <schluet0r4life@gmail.com>
2025-04-14 18:29:59 +0200

    Obfuscate Secrets

--- src/app/(authenticated)/organization/[organizationId]/(admin)/credentials/page.tsx
+++ src/app/(authenticated)/organization/[organizationId]/(admin)/credentials/page.tsx
@@ -2,6 +2,9 @@ import Heading from "@/components/heading";
 import OrganizationBreadcrumbs from "../_navigation/organization-breadcrumbs";
 import { getOrganizationName } from "@/features/organization/queries/get-organization-name";
 import CredentialCreateButton from "@/features/credential/components/credential-create-button";
+import { Suspense } from "react";
+import Spinner from "@/components/spinner";
+import CredentialList from "@/features/credential/components/credential-list";
 
 type CredentialsPageProps = {
   params: Promise<{
@@ -23,6 +26,10 @@ export default async function CredentialsPage({
         tabs={<OrganizationBreadcrumbs organizationName={organizationName} />}
         actions={<CredentialCreateButton organizationId={organizationId} />}
       />
+
+      <Suspense fallback={<Spinner />}>
+        <CredentialList organizationId={organizationId} />
+      </Suspense>
     </div>
   );
 }
---
+++ src/features/credential/components/credential-list.tsx
@@ -0,0 +1,58 @@
+import Placeholder from "@/components/placeholder";
+import { getCredentials } from "../queries/get-credentials";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { format } from "date-fns";
+
+type CredentialListProps = {
+  organizationId: string;
+};
+
+export default async function CredentialList({
+  organizationId,
+}: CredentialListProps) {
+  const credentials = await getCredentials(organizationId);
+
+  if (!credentials.length) {
+    return <Placeholder label="No credentials for this organization" />;
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Created At</TableHead>
+          <TableHead>Last Used</TableHead>
+          <TableHead />
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {credentials.map((credential) => {
+          const buttons = <></>; // TODO: add revoke credential button
+
+          return (
+            <TableRow key={credential.id}>
+              <TableCell>{credential.name}</TableCell>
+              <TableCell>
+                {format(credential.createdAt, "yyyy-MM-dd, HH:mm")}
+              </TableCell>
+              <TableCell>
+                {credential.lastUsed
+                  ? format(credential.lastUsed, "yyyy-MM-dd, HH:mm")
+                  : "Never"}
+              </TableCell>
+              <TableCell>{buttons}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
