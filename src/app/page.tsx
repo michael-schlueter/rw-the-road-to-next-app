e0f8055a9c57bab8d423529d01f5a04637d69681
rulo <schluet0r4life@gmail.com>
2025-05-12 17:49:41 +0200

    fix(org-filter): add conditional props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default async function HomePage({ searchParams }: HomePageProps) {
       <Suspense fallback={<Spinner />}>
         <TicketList
           searchParams={searchParamsCache.parse(await searchParams)}
+          byOrganization={false}
         />
       </Suspense>
     </div>
--- src/features/ticket/components/ticket-org-filter-switch.tsx
+++ src/features/ticket/components/ticket-org-filter-switch.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useQueryState } from "nuqs";
 import { orgFilterOptions, orgFilterParser } from "../search-params";
 import { Switch } from "@/components/ui/switch";
