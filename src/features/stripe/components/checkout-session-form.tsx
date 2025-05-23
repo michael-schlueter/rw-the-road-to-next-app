289d179d1d2381a4ae8a5e79fb15b4f9c8e06d60
rulo <schluet0r4life@gmail.com>
2025-05-23 12:12:53 +0200

    Checkout Session

---
+++ src/features/stripe/components/checkout-session-form.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Form from "@/components/form/form";
+import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
+import { Button } from "@/components/ui/button";
+import { useActionState } from "react";
+import { createCheckoutSession } from "../actions/create-checkout-session";
+
+type CheckoutSessionFormProps = {
+  organizationId: string | null | undefined;
+  priceId: string;
+  children: React.ReactNode;
+};
+
+export default function CheckoutSessionForm({
+  organizationId,
+  priceId,
+  children,
+}: CheckoutSessionFormProps) {
+  const [actionState, action] = useActionState(
+    createCheckoutSession.bind(null, organizationId, priceId),
+    EMPTY_ACTION_STATE
+  );
+  return (
+    <Form action={action} actionState={actionState}>
+      <Button type="submit">{children}</Button>
+    </Form>
+  );
+}
