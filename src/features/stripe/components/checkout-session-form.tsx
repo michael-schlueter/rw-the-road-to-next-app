e7aa14df096b938020eddf429e634c1fe547997a
rulo <schluet0r4life@gmail.com>
2025-06-10 16:06:22 +0200

    (feature): add confirmation dialog

--- src/features/stripe/components/checkout-session-form.tsx
+++ src/features/stripe/components/checkout-session-form.tsx
@@ -6,8 +6,82 @@ import { Button } from "@/components/ui/button";
 import { useActionState } from "react";
 import { createCheckoutSession } from "../actions/create-checkout-session";
 import clsx from "clsx";
-// import { createCustomerPortal } from "../actions/create-customer-portal";
 import updateSubscription from "../actions/update-subscription";
+import useConfirmDialog from "@/components/confirm-dialog";
+import { LucideLoaderCircle } from "lucide-react";
+
+type NewSubscriptionFormProps = {
+  organizationId: string | null | undefined;
+  priceId: string;
+  children: React.ReactNode;
+};
+
+function NewSubscriptionForm({
+  organizationId,
+  priceId,
+  children,
+}: NewSubscriptionFormProps) {
+  const [actionState, action] = useActionState(
+    createCheckoutSession.bind(null, organizationId, priceId),
+    EMPTY_ACTION_STATE
+  );
+
+  return (
+    <Form action={action} actionState={actionState}>
+      <Button type="submit" className="flex flex-col">
+        <div>{children}</div>
+      </Button>
+    </Form>
+  );
+}
+
+type UpdateSubscriptionButtonWithDialogProps = {
+  organizationId: string | null | undefined;
+  priceId: string;
+  activePriceId: string;
+  children: React.ReactNode;
+};
+
+function UpdateSubscriptionButtonWithDialog({
+  organizationId,
+  priceId,
+  activePriceId,
+  children,
+}: UpdateSubscriptionButtonWithDialogProps) {
+  const isActivePrice = activePriceId === priceId;
+
+  const [dialogTrigger, dialog] = useConfirmDialog({
+    title: "Confirm Subscription Update",
+    description:
+      "Are you sure you want to change your subscription plan? This may incur pro-rated charges or credits.",
+    action: updateSubscription.bind(null, organizationId, priceId),
+    trigger: (isDialogActionPending) => (
+      <Button
+        type="button"
+        disabled={isActivePrice || isDialogActionPending}
+        className={clsx("flex flex-col", {
+          "h-16": true,
+        })}
+      >
+        {isDialogActionPending ? (
+          <LucideLoaderCircle className="mx-auto h-6 w-6 animate-spin" />
+        ) : isActivePrice ? (
+          <span>Current Plan</span>
+        ) : (
+          <span>Change Plan</span>
+        )}
+        <div>{children}</div>
+      </Button>
+    ),
+  });
+
+  return (
+    <>
+      {dialogTrigger}
+      {dialog}
+    </>
+  );
+}
 
 type CheckoutSessionFormProps = {
   organizationId: string | null | undefined;
@@ -22,34 +96,21 @@ export default function CheckoutSessionForm({
   activePriceId,
   children,
 }: CheckoutSessionFormProps) {
-  console.log(process.env.STRIPE_SECRET_KEY)
-  const isActivePrice = activePriceId === priceId;
-  
-  
-
-  const [actionState, action] = useActionState(
-    !activePriceId
-      ? createCheckoutSession.bind(null, organizationId, priceId)
-      : updateSubscription.bind(null, organizationId, priceId),
-    EMPTY_ACTION_STATE
-  );
-
-  return (
-    <Form action={action} actionState={actionState}>
-      <Button
-        type="submit"
-        disabled={isActivePrice}
-        className={clsx("flex flex-col", {
-          "h-16": !!activePriceId,
-        })}
+  if (!activePriceId) {
+    return (
+      <NewSubscriptionForm organizationId={organizationId} priceId={priceId}>
+        {children}
+      </NewSubscriptionForm>
+    );
+  } else {
+    return (
+      <UpdateSubscriptionButtonWithDialog
+        organizationId={organizationId}
+        priceId={priceId}
+        activePriceId={activePriceId}
       >
-        {!activePriceId ? null : isActivePrice ? (
-          <span>Current Plan</span>
-        ) : (
-          <span>Change Plan</span>
-        )}
-        <div>{children}</div>
-      </Button>
-    </Form>
-  );
+        {children}
+      </UpdateSubscriptionButtonWithDialog>
+    );
+  }
 }
