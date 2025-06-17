341e7aeeabe7d13a1c0c9ad570f86d5b8f9ee35e
rulo <schluet0r4life@gmail.com>
2025-06-17 16:46:15 +0200

    Enforce private tickets on server

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -2,9 +2,17 @@ import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { prisma } from "@/lib/prisma";
 import { getTicketPermissions } from "../permissions/get-ticket-permission";
+import { getActiveOrganization } from "@/features/organization/queries/get-active-organization";
+import { getStripeCustomberByOrganization } from "@/features/stripe/queries/get-stripe-customer";
+import { isActiveSubscription } from "@/features/stripe/utils/is-active-subscription";
 
 export default async function getTicket(id: string) {
   const { user } = await getAuth();
+    const activeOrganization = await getActiveOrganization();
+    const stripeCustomer = await getStripeCustomberByOrganization(
+      activeOrganization?.id
+    );
+    const hasActiveSubscription = await isActiveSubscription(stripeCustomer);
 
   const ticket = await prisma.ticket.findUnique({
     where: {
@@ -19,7 +27,7 @@ export default async function getTicket(id: string) {
     },
   });
 
-  if (!ticket) return null;
+  if (!ticket || (ticket.private && !hasActiveSubscription)) return null;
 
   const permissions = await getTicketPermissions({
     organizationId: ticket.organizationId,
