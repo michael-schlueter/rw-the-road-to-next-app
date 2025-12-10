163eb688bfdafe3a01ee533463708f0582eeabe0
rulo <schluet0r4life@gmail.com>
2025-12-10 14:31:06 +0100

    update s3 variable names

--- environment.d.ts
+++ environment.d.ts
@@ -2,8 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
-      AWS_ACCESS_KEY: string;
-      AWS_SECRET_ACCESS_KEY: string;
+      S3_ACCESS_KEY: string;
+      S3_SECRET_ACCESS_KEY: string;
       AWS_BUCKET_NAME: string;
       AWS_REGION: string;
       STRIPE_SECRET_KEY: string;
